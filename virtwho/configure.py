ca0a86388c865b7e52c0683336c93868a9faafa0
Yuefen Liu <yuefliu@redhat.com>
2021-03-18 17:00:26 +0800

    a whole update

--- virtwho/configure.py
+++ virtwho/configure.py
@@ -1,35 +1,119 @@
-"""
-Used to manage virt-who configure file
+"""Used to manage virt-who configure file
 Add/Delete/Update options for /etc/virt-who.conf, /etc/virt-who.d/xxx.conf
 """
 from virtwho.settings import Configure
+from virtwho.settings import config
+from virtwho.ssh import SSHConnect
+from virtwho.logger import getLogger
+
+logger = getLogger(__name__)
 
 
 class VirtwhoHypervisorConfig:
-    def __init__(self, mode):
-        config = Configure('properties.ini')
-        self.remote_ssh = {'host': config.get('virtwho', 'host'),
-                           'user': config.get('virtwho', 'username'),
-                           'pwd': config.get('virtwho', 'password')}
-        self.local_file = f"{mode}.conf"
-        self.remote_file = f"/etc/virt-who.d/{mode}.conf"
+    """Able to create and manage /etc/virt-who.d/xxx.conf file when
+    call this class"""
+    def __init__(self, mode, register):
+        """Create virt-who configuration file with basic options.
+         All data come from virtwho.ini file.
+        :param mode: The hypervisor mode.
+            (esx, xen, hyperv, rhevm, libvirt, kubevirt, local)
+        :param register: The subscription server. (rhsm, satellite)
+        """
+        self.remote_ssh = virtwho_ssh_connect(mode)
+        self.local_file = f'/root/{mode}.conf'
+        self.remote_file = f'/etc/virt-who.d/{mode}.conf'
         self.section = f'virtwho-{mode}'
         self.cfg = Configure(self.local_file, self.remote_ssh, self.remote_file)
-
-    def get(self, option):
-        """Read an option from a section"""
-        return self.cfg.get(self.section, option)
+        if mode == 'local':
+            logger.info("Don't need to configure for local mode")
+        else:
+            self.update('type', mode)
+            if mode == 'esx':
+                self.update('server', config.vcenter.server)
+                self.update('username', config.vcenter.username)
+                self.update('password', config.vcenter.password)
+            if mode == 'xen':
+                self.update('server', config.xen.server)
+                self.update('username', config.xen.username)
+                self.update('password', config.xen.password)
+            if mode == 'hyperv':
+                self.update('server', config.hyperv.server)
+                self.update('username', config.hyperv.username)
+                self.update('password', config.hyperv.password)
+            if mode == 'rhevm':
+                self.update('server', config.rhevm.server)
+                self.update('username', config.rhevm.username)
+                self.update('password', config.rhevm.password)
+            if mode == 'libvirt':
+                self.update('server', config.libvirt.server)
+                self.update('username', config.libvirt.username)
+                self.update('password', config.libvirt.password)
+            if mode == 'kubevirt':
+                self.update('kubeconfig', config.mode.config_file)
+            if register == "rhsm":
+                self.update('owner', config.rhsm.default_org)
+            else:
+                self.update('owner', config.satellite.default_org)
+            self.update('hypervisor_id', 'hostname')
 
     def update(self, option, value):
-        """Update an option's value."""
+        """Add or update an option
+        :param option: Option will be added if not exist.
+        :param value: Value to update for the option.
+        """
         self.cfg.update(self.section, option, value)
-        self.cfg.put()
 
     def delete(self, option):
-        """Delete an option"""
+        """Delete an option
+        :param option: Option to remove.
+        """
         self.cfg.delete(self.section, option)
-        self.cfg.put()
 
     def destroy(self):
-        """Delete a file"""
-        self.cfg.rm_remote_file()
+        """Remove the /etc/virt-who.d/{mode}.conf file"""
+        self.remote_ssh.remove_file(self.remote_file)
+
+
+class VirtwhoGlobalConfig:
+    """Able to manage /etc/virt-who.conf file when call this class"""
+    def __init__(self, mode=None):
+        """
+        :param mode: Hypervisor mode. When mode is local, will manage
+            the local libvirt host, othervise will manage the host for
+            all other remote modes.
+        """
+        self.remote_ssh = virtwho_ssh_connect(mode)
+        self.local_file = '/root/virt-who.conf'
+        self.remote_file = '/etc/virt-who.conf'
+        self.cfg = Configure(self.local_file, self.remote_ssh, self.remote_file)
+
+    def update(self, section, option, value):
+        """Add section, add option or update option
+        :param section: Section will be added if not exist.
+        :param option: Option will be added if not exist.
+        :param value: Value to update for the option.
+        """
+        self.cfg.update(section, option, value)
+
+    def delete(self, section, option=None):
+        """Remove a section or option
+        :param section: Section will be removed when no option provided.
+        :param option: Option to remove.
+        """
+        self.cfg.delete(section, option)
+
+
+def virtwho_ssh_connect(mode=None):
+    """Define the ssh connection of virt-who host, get data from
+    virtwho.ini file.
+    :param mode: The test hypervisor mode.
+    """
+    if mode == 'local':
+        host = config.local.host
+        username = config.local.username
+        password = config.local.password
+    else:
+        host = config.virtwho.host
+        username = config.virtwho.username
+        password = config.virtwho.password
+    return SSHConnect(host, user=username, pwd=password)
