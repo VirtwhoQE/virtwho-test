c2417ceffb3d73c55dce14681c7cef6b172ef1f2
Yuefen Liu <yuefliu@redhat.com>
2021-03-19 11:02:29 +0800

    remove useless comment

--- virtwho/configure.py
+++ virtwho/configure.py
@@ -1,6 +1,3 @@
-"""Used to manage virt-who configure file
-Add/Delete/Update options for /etc/virt-who.conf, /etc/virt-who.d/xxx.conf
-"""
 from virtwho.settings import Configure
 from virtwho.settings import config
 from virtwho.ssh import SSHConnect
