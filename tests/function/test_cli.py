1696805708c162f3da9ac66284d936f0a50fce96
Yuefen Liu <yuefliu@redhat.com>
2021-04-15 18:15:59 +0800

    update the demo in test_cli.py

--- tests/function/test_cli.py
+++ tests/function/test_cli.py
@@ -21,56 +21,39 @@ class TestCli:
         """
 
         # run without '-d'
-        tty_output = runner.run_virtwho_cli(debug=False)
-        assert runner.log_analyzer(tty_output)
-        assert runner.msg_search(tty_output, "\\[.*DEBUG\\]") is False
+        log = runner.run_virtwho_cli(debug=False)
+        assert (log["send_number"] == 1 and log["debug"] is False)
 
         # run with '-d'
-        tty_output = runner.run_virtwho_cli(debug=True)
-        assert runner.log_analyzer(tty_output)
-        assert runner.msg_search(tty_output, "\\[.*DEBUG\\]") is True
+        log = runner.run_virtwho_cli(debug=True, oneshot=False)
+        assert (log["send_number"] == 1 and log["debug"] is True)
 
     def test_oneshot_in_cli(self):
         """test the '-o' option in command line
         """
 
         # run without '-o'
-        tty_output = runner.run_virtwho_cli(oneshot=False)
-        assert runner.log_analyzer(tty_output)
-        assert runner.msg_search(tty_output, "virt-who terminated") is False
+        log = runner.run_virtwho_cli(oneshot=False)
+        assert (log["send_number"] == 1 and log["oneshot"] is False)
 
         # run with '-o'
-        tty_output = runner.run_virtwho_cli(oneshot=True)
-        assert runner.log_analyzer(tty_output)
-        assert runner.msg_search(tty_output, "virt-who terminated") is True
+        log = runner.run_virtwho_cli(oneshot=True)
+        assert (log["send_number"] == 1 and log["oneshot"] is True)
 
     def test_interval_in_cli(self):
         """test the '-i ' option in command line
         """
 
         # run without '-i' to test default interval=3600
-        tty_output = runner.run_virtwho_cli(oneshot=False, interval=None)
-        assert runner.log_analyzer(tty_output, interval_time=3600)
+        log = runner.run_virtwho_cli(oneshot=False, interval=None)
+        assert (log["send_number"] == 1 and log["interval_time"] == 3600)
 
         # run with '-i 10' and check interval will use default 3600
-        tty_output = runner.run_virtwho_cli(oneshot=False, interval=10)
-        assert runner.log_analyzer(tty_output, interval_time=3600)
+        log = runner.run_virtwho_cli(oneshot=False, interval=10)
+        assert (log["send_number"] == 1 and log["interval_time"] == 3600)
 
         # run with '-i 60' to test interval=60
-        tty_output = runner.run_virtwho_cli(oneshot=False, interval=60, wait=60)
-        assert runner.log_analyzer(tty_output, interval_time=60, loop_time=60)
-
-    def test_oneshot_and_interval_in_cli(self):
-        pass
-
-    def test_print_in_cli(self):
-        pass
-
-    def test_config_in_cli(self):
-        pass
-
-    def test_help_in_cli(self):
-        pass
-
-    def test_version_in_cli(self):
-        pass
+        log = runner.run_virtwho_cli(oneshot=False, interval=60, wait=60)
+        assert (log["send_number"] == 1
+                and log["interval_time"] == 60
+                and log["loop_time"] == 60)
