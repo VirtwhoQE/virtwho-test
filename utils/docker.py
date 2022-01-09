02581ba7ea51bc6a0d21dd015eccb1eedf8928d6
YuefenLiu <yuefliu@redhat.com>
2022-01-09 11:07:07 +0800

    update

--- utils/docker.py
+++ utils/docker.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python
 import os
 import random
-import re
-import sys
-import time
 import argparse
 
-
 from virtwho import base, logger, FailException
-from virtwho.settings import config
 from virtwho.ssh import SSHConnect
 
 
@@ -26,15 +21,14 @@ def create_rhel_docker_container(args):
         host_require: optional, other hostRequires for job,
             separate multiple options with commas.
     """
-    conf_hosts = dict()
     ssh_docker = SSHConnect(
         host=args.docker_server,
         user=args.docker_username,
         pwd=args.docker_password
     )
-    image_name = args.rhel_compose.lower()
     # clean docker cache
     ssh_docker.runcmd('docker system prune -f')
+    # copy docker files to docker server
     cur_path = os.path.abspath(os.path.dirname(__file__))
     root_path = os.path.split(cur_path)[0]
     local_dir = os.path.join(root_path, 'docker/')
@@ -45,26 +39,38 @@ def create_rhel_docker_container(args):
     ssh_docker.runcmd('subscription-manager unregister;'
                       'subscription-manager clean')
     ssh_docker.put_dir(local_dir, remote_dir)
-    docker_image_create(ssh_docker, args.rhel_compose)
-    container_port = args.container_port or docker_container_port(ssh_docker)
-    container_name = image_name.replace('.', '-') + '-' + container_port
-    if docker_container_create(
-            ssh_docker, image_name, container_name, container_user,
-            container_passwd, container_port):
-        ip_value = "{0}:{1}".format(server, container_port)
-        ip_name = "virtwho-host-{0}-ip".format(mode)
-        conf_hosts[ip_name] = ip_value
-        ssh_container = {"host": ip_value, "username": container_user,
-                         "password": container_passwd}
-        self.rhsm_override_uuid(ssh_container)
-        if mode == "libvirt-remote":
-            self.ssh_no_passwd_access(ssh_container)
-    time.sleep(60)
-    return conf_hosts
+    # Check if the container name already exists
+    image_name = args.rhel_compose.lower()
+    container_port = (args.container_port
+                      or
+                      docker_container_port(ssh_docker))
+    container_name = (args.container_name
+                      or
+                      docker_container_name(image_name, container_port))
+    if docker_container_exist(ssh_docker, container_name):
+        logger.warning(f'The docker container {container_name} already exists')
+        return
+    # Create docker image and container
+    docker_image_create(ssh_docker, image_name, args.rhel_compose)
+    if docker_container_create(ssh_docker, image_name, container_name,
+                               container_port, args.container_username,
+                               args.container_password):
+        ssh_container = SSHConnect(
+            host=args.docker_server,
+            user=args.container_username,
+            pwd=args.container_password,
+            port=container_port
+        )
+        if base.ssh_connect(ssh_container):
+            logger.info(
+                f'Succeeded to create docker container ({container_name}), '
+                f'the ip is {args.docker_server}:{container_port}'
+            )
+            return args.docker_server, container_port
+    raise FailException(f'Failed to create docker container {container_name}.')
 
 
-def docker_image_create(ssh, compose_id):
-    image_name = compose_id.lower()
+def docker_image_create(ssh, image_name, compose_id):
     if not docker_image_exist(ssh, image_name):
         repo_file = '/tmp/docker/compose.repo'
         base.rhel_compose_repo(ssh, compose_id, repo_file)
@@ -97,14 +103,26 @@ def docker_container_port(ssh):
     return str(port)
 
 
-def docker_container_create(ssh, image_name, cont_name, cont_user, cont_passwd, cont_port):
-    cmd = "sh /tmp/docker/mk_container.sh -i {0} -c {1} -o {2} -u {3} -p {4}"\
-            .format(image_name, cont_name, cont_port, cont_user, cont_passwd)
-    ssh.runcmd(cmd)
-    if docker_container_exist(ssh, cont_port) and docker_container_exist(ssh, cont_name):
-        logger.info("Succeeded to create container: {0}:{1}".format(cont_name, cont_port))
+def docker_container_name(image_name, container_port):
+    name = (image_name.replace('.', '-')
+            + '-'
+            + container_port)
+    return name
+
+
+def docker_container_create(ssh, image_name, container_name, container_port,
+                            container_username, container_password):
+    ssh.runcmd(
+        f'sh /tmp/docker/mk_container.sh '
+        f'-i {image_name} '
+        f'-c {container_name} '
+        f'-o {container_port} '
+        f'-u {container_username} '
+        f'-p {container_password}'
+    )
+    if docker_container_exist(ssh, container_name):
         return True
-    raise FailException(f'Failed to create container {cont_name}')
+    return False
 
 
 def docker_arguments_parser():
