48c8ce46cb0843eb30d47a7442ba38d5c8ad0a6e
Juneliu <yuefliu@redhat.com>
2022-06-09 21:03:19 +0800

    Define test cases of test_cli.py (#64)
    * add new fixture rhel_compose, mode_file and ssh_host
    
    * add data['log'] in analyzer() function
    
    * define test_cli.py
    
    * add new marks
    
    * define global variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,25 +2,21 @@ from virtwho.settings import config
 from virtwho.runner import VirtwhoRunner
 from virtwho.configure import VirtwhoHypervisorConfig
 from virtwho.configure import VirtwhoGlobalConfig
-from virtwho.configure import get_hypervisor_handler
+from virtwho.configure import get_hypervisor_handler, virtwho_ssh_connect
 from virtwho.configure import get_register_handler
 from virtwho.register import SubscriptionManager
-from virtwho import logger
+from virtwho import HYPERVISOR, REGISTER
 import pytest
-
-
-mode = config.job.hypervisor
-register_type = config.job.register
 
 
 @pytest.fixture(name='hypervisor_handler', scope='session')
 def virtwho_hypervisor_handler():
-    return get_hypervisor_handler(mode)
+    return get_hypervisor_handler(HYPERVISOR)
 
 
 @pytest.fixture(name='hypervisor', scope='session')
 def virtwho_hypervisor_config():
-    return VirtwhoHypervisorConfig(mode, register_type)
+    return VirtwhoHypervisorConfig(HYPERVISOR, REGISTER)
 
 
 @pytest.fixture(name='hypervisor_create', scope='class')
@@ -30,12 +26,12 @@ def virtwho_hypervisor_config_file_create(hypervisor):
 
 @pytest.fixture(name='register_handler', scope='session')
 def virtwho_register_handler():
-    return get_register_handler(register_type)
+    return get_register_handler(REGISTER)
 
 
 @pytest.fixture(name='globalconf', scope='session')
 def virtwho_global_config():
-    return VirtwhoGlobalConfig(mode)
+    return VirtwhoGlobalConfig(HYPERVISOR)
 
 
 @pytest.fixture(name='globalconf_clean', scope='function')
@@ -50,7 +46,12 @@ def virtwho_global_config_debug_true(globalconf):
 
 @pytest.fixture(name='virtwho', scope='class')
 def virtwho_runner():
-    return VirtwhoRunner(mode, register_type)
+    return VirtwhoRunner(HYPERVISOR, REGISTER)
+
+
+@pytest.fixture(name='ssh_host', scope='session')
+def virtwho_host_ssh_connect():
+    return virtwho_ssh_connect(HYPERVISOR)
 
 
 @pytest.fixture(name='sm_host', scope='session')
@@ -59,7 +60,7 @@ def subscription_manager_virtwho_host_with_default_org(register_handler):
     vw_username = config.virtwho.username
     vw_password = config.virtwho.password
     vw_port = config.virtwho.port or 22
-    if mode == 'local':
+    if HYPERVISOR == 'local':
         vw_server = config.local.server
         vw_username = config.local.username
         vw_password = config.local.password
@@ -68,7 +69,7 @@ def subscription_manager_virtwho_host_with_default_org(register_handler):
                                username=vw_username,
                                password=vw_password,
                                port=vw_port,
-                               register_type=register_type,
+                               register_type=REGISTER,
                                org=register_handler.default_org)
 
 
@@ -78,7 +79,7 @@ def subscription_manager_guest_host_with_default_org(hypervisor_handler, registe
                                username=hypervisor_handler.guest_username,
                                password=hypervisor_handler.guest_password,
                                port=22,
-                               register_type=register_type,
+                               register_type=REGISTER,
                                org=register_handler.default_org)
 
 
@@ -89,11 +90,11 @@ def hypervisor_common_data_get_from_virtwho_ini(hypervisor_handler):
     data['hypervisor_uuid'] = ''
     data['hypervisor_hostname'] = ''
     data['hypervisor_hwuuid'] = ''
-    if mode == 'esx':
+    if HYPERVISOR == 'esx':
         data['hypervisor_uuid'] = hypervisor_handler.esx_uuid
         data['hypervisor_hostname'] = hypervisor_handler.esx_hostname
         data['hypervisor_hwuuid'] = hypervisor_handler.esx_hwuuid
-    elif mode == 'rhevm':
+    elif HYPERVISOR == 'rhevm':
         data['hypervisor_uuid'] = hypervisor_handler.vdsm_uuid
         data['hypervisor_hostname'] = hypervisor_handler.vdsm_hostname
         data['hypervisor_hwuuid'] = hypervisor_handler.vdsm_hwuuid
