97f7d86b9145da341f41b8d1d33d1512c79f8d66
Juneliu <yuefliu@redhat.com>
2022-06-29 15:42:23 +0800

    Define test_install_uninstall.py (#68)
    * define test_install_uninstall.py
    
    * update the virtwho_package_url() function
    
    * update case id

--- tests/package/test_install_uninstall.py
+++ tests/package/test_install_uninstall.py
@@ -5,23 +5,60 @@
 :caseautomation: Automated
 """
 import pytest
-from virtwho import logger
+from virtwho import VIRTWHO_PKG
+from virtwho.base import package_check, package_install, package_uninstall
+from virtwho.base import wget_download, random_string
+from virtwho.testing import virtwho_pacakge_url
 
 
 class TestInstallUninstall:
     @pytest.mark.tier1
-    def test_install_uninstall_by_yum(self):
-        """Just a demo
+    def test_install_uninstall_by_yum(self, ssh_host):
+        """Test virt-who install/uninstall by yum
 
-        :title: VIRT-WHO: test install uninstall by yum
-        :id: d2b53819-e876-4595-8407-549eb8ef1bdb
+        :title: virt-who: package: install/uninstall by yum
+        :id: 7b32612a-11eb-437e-92c9-b7d501d2e8a0
         :caseimportance: High
         :tags: tier1
         :customerscenario: false
         :upstream: no
         :steps:
-            1.
+            1. uninstall virt-who by #yum remove virt-who
+            2. install virt-who by #yum install virt-who
         :expectedresults:
-            1.
+            1. virt-who can be remove and reinstall successfully.
         """
-        logger.info("Succeeded to run the 'test_install_uninstall'")
+        package_uninstall(ssh_host, 'virt-who')
+        assert package_check(ssh_host, 'virt-who') is False
+        package_install(ssh_host, 'virt-who')
+        assert package_check(ssh_host, 'virt-who') == VIRTWHO_PKG
+
+    @pytest.mark.tier1
+    def test_install_uninstall_by_rpm(self, ssh_host):
+        """Test virt-who install/uninstall by rpm
+
+        :title: virt-who: package: install/uninstall by rpm
+        :id: 14152159-22a7-4550-9281-1109c4440f34
+        :caseimportance: High
+        :tags: tier1
+        :customerscenario: false
+        :upstream: no
+        :steps:
+            1. uninstall virt-who by #rpm -e
+            2. install virt-who by #rpm -ivh
+        :expectedresults:
+            1. virt-who can be remove and reinstall successfully.
+        """
+        try:
+            package_uninstall(ssh_host, 'virt-who', rpm=VIRTWHO_PKG)
+            assert package_check(ssh_host, 'virt-who') is False
+
+            pkg_url = virtwho_pacakge_url(VIRTWHO_PKG)
+            file_path = '/root/' + random_string()
+            wget_download(ssh_host, url=pkg_url, file_path=file_path)
+            package_install(ssh_host, 'virt-who',
+                            rpm=f'{file_path}/{VIRTWHO_PKG}.rpm')
+            assert package_check(ssh_host, 'virt-who') == VIRTWHO_PKG
+        finally:
+            if package_check(ssh_host, 'virt-who') is False:
+                package_install(ssh_host, 'virt-who')
