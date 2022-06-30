eb4e5e448e0fc6b2dbd9af60a19acb21a934cdf7
hkx303 <kuhuang@redhat.com>
2022-06-30 17:28:46 +0800

    update the error message for satellite

--- tests/conftest.py
+++ tests/conftest.py
@@ -235,5 +235,5 @@ def owner_data():
     owner['owner'] = register_handler.default_org
     owner['bad_owner'] = bad_owner
     owner['error'] = [f'Organization with id {bad_owner} could not be found',
-                      f'Organization with id {bad_owner} could not be found']
+                      f"Couldn't find Organization '{bad_owner}'"]
     return owner
