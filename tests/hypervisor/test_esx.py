12937998ed0bee4ccb2344432293e4be0b00b6b9
hkx303 <kuhuang@redhat.com>
2022-08-24 21:41:21 +0800

    add test_type, test_owner and test_hypervisor_id cases to test_esx.py

--- tests/hypervisor/test_esx.py
+++ tests/hypervisor/test_esx.py
@@ -6,10 +6,140 @@
 """
 import pytest
 from virtwho import logger
+from virtwho import REGISTER
+from virtwho import RHEL_COMPOSE
 
 
+@pytest.mark.usefixtures('globalconf_clean')
+@pytest.mark.usefixtures('hypervisor_create')
+@pytest.mark.usefixtures('debug_true')
 class TestEsx:
     @pytest.mark.tier1
+    def test_type(self, virtwho, hypervisor, hypervisor_data):
+        """Test the type= option in /etc/virt-who.d/hypervisor.conf
+
+        :title: virt-who: esx: test type option
+        :id: 0f2fcf4f-ac60-4c3c-905a-fabe22536ab2
+        :caseimportance: High
+        :tags: tier1
+        :customerscenario: false
+        :upstream: no
+        :steps:
+            1.
+        :expectedresults:
+            1.
+        """
+        # type option is wrong value
+        validate_values = ['xxx', '红帽€467aa', '']
+        for value in validate_values:
+            hypervisor.update('type', value)
+            result = virtwho.run_service()
+            assert (result['error'] is not 0
+                    and result['send'] == 0
+                    and result['thread'] == 1
+                    and "virt-who can't be started" in result['error_msg'])
+            if 'RHEL-9' in RHEL_COMPOSE:
+                assert f"Unsupported virtual type '{value}' is set" in result['error_msg']
+            else:
+                assert "virt-who can't be started" in result['error_msg']
+
+        # type option is disable
+        hypervisor.delete('type')
+        hypervisor.update('#type', '')
+        result = virtwho.run_service()
+        assert (result['error'] is not 0
+                and result['send'] == 0
+                and result['thread'] == 1
+                and "Error in .* backend" in result['error_msg'])
+
+        # type option is disable but another config is ok
+
+        # type option is null but another config is ok
+
+    @pytest.mark.tier1
+    def test_owner(self, virtwho, hypervisor, hypervisor_data, owner_data):
+        """Test the owner= option in /etc/virt-who.d/hypervisor.conf
+
+        :title: virt-who: esx: test owner option
+        :id: 0f2fcf4f-ac60-4c3c-905a-fabe22536ab2
+        :caseimportance: High
+        :tags: tier1
+        :customerscenario: false
+        :upstream: no
+        :steps:
+            1.
+        :expectedresults:
+            1.
+        """
+        # owner option is wrong value
+        validate_values = ['xxxxxx', '']
+        for value in validate_values:
+            hypervisor.update('owner', value)
+            result = virtwho.run_service()
+            assert (result['error'] is not 0
+                    and result['send'] == 0
+                    and result['thread'] == 1
+                    and any(error in result['error_msg'] for error in owner_data['error']))
+
+        # owner option is disable
+        hypervisor.delete('owner')
+        hypervisor.update('#owner', '')
+        result = virtwho.run_service()
+        assert (result['error'] is not 0
+                and result['send'] == 0
+                and result['thread'] == 1
+                and any(error in result['error_msg'] for error in owner_data['error']))
+
+        # owner option is disable but another config is ok
+
+        # owner option is null but another config is ok
+
+    @pytest.mark.tier1
+    def test_hypervisor_id(self, virtwho, hypervisor, hypervisor_data, globalconf, rhsm, satellite):
+        """Test the hypervisor_id= option in /etc/virt-who.d/hypervisor.conf
+
+        :title: virt-who: esx: test hypervisor_id function
+        :id: be5877d9-3a59-46aa-bd9a-6c1e3ed5f5ee
+        :caseimportance: High
+        :tags: tier1
+        :customerscenario: false
+        :upstream: no
+        :steps:
+
+            1. clean all virt-who global configurations
+            2. run virt-who with hypervisor_id=uuid
+            3. run virt-who with hypervisor_id=hostname
+            4. run virt-who with hypervisor_id=hwuuid
+
+        :expectedresults:
+
+            hypervisor id shows uuid/hostname/hwuuid in mapping as the setting.
+        """
+        hypervisor_ids = ['hostname', 'uuid', 'hwuuid']
+        for hypervisor_id in hypervisor_ids:
+            hypervisor.update('hypervisor_id', hypervisor_id)
+            result = virtwho.run_service()
+            assert (result['error'] == 0
+                    and result['send'] == 1
+                    and result['thread'] == 1
+                    and result['hypervisor_id'] == hypervisor_data[f'hypervisor_{hypervisor_id}'])
+            if REGISTER == 'rhsm':
+                assert rhsm.consumers(hypervisor_data['hypervisor_hostname'])
+                rhsm.delete(hypervisor_data['hypervisor_hostname'])
+            else:
+                if hypervisor_id == 'hostname':
+                    assert satellite.host_id(hypervisor_data['hypervisor_hostname'])
+                    assert not satellite.host_id(hypervisor_data['hypervisor_uuid'])
+                    assert not satellite.host_id(hypervisor_data['hypervisor_hwuuid'])
+                elif hypervisor_id == 'uuid':
+                    assert satellite.host_id(hypervisor_data['hypervisor_uuid'])
+                    assert not satellite.host_id(hypervisor_data['hypervisor_hostname'])
+                    assert not satellite.host_id(hypervisor_data['hypervisor_hwuuid'])
+                else:
+                    assert satellite.host_id(hypervisor_data['hypervisor_hwuuid'])
+                    assert not satellite.host_id(hypervisor_data['hypervisor_hostname'])
+                    assert not satellite.host_id(hypervisor_data['hypervisor_uuid'])
+
     def test_hostname_option(self):
         """Just a demo
 
