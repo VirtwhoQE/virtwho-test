bb4abd92fa2d89dd3e79d5d2db13ab34ea732d70
YuefenLiu <yuefliu@redhat.com>
2022-09-27 10:44:25 +0800

    remove useless lines

--- virtwho/provision/virtwho_hypervisor.py
+++ virtwho/provision/virtwho_hypervisor.py
@@ -8,12 +8,11 @@ curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(os.path.split(rootPath)[0])
 
-from virtwho import logger, FailException
+from virtwho import logger
 from virtwho.settings import config
 from virtwho.ssh import SSHConnect
 from virtwho.base import hostname_get, ipaddr_get, host_ping
 from utils.properties_update import virtwho_ini_update
-from utils.properties_update import virtwho_ini_props_update
 from hypervisor.virt.libvirt.libvirtcli import LibvirtCLI
 from hypervisor.virt.esx.powercli import PowerCLI
 from hypervisor.virt.hyperv.hypervcli import HypervCLI
