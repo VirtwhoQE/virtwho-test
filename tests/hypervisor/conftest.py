8076e4913c4383c5f2ba04cafcf9e5d34ade56f5
Kunxin Huang <kuhuang@redhat.com>
2022-11-01 09:40:38 +0800

    Create the test_rhesm.py cloning the test_esx.py (#100)
    * create the test_rhesm.py cloning the test_esx.py
    
    * update test_fake_type case for rhevm mode
    
    * add the test_type negative case for rhevm mode
    
    * add test_server, test_username, test_password cases for rhevm hypervisor
    
    * add test_encrypted_password test cases for rhevm hypervisor

--- tests/hypervisor/conftest.py
+++ tests/hypervisor/conftest.py
@@ -60,3 +60,64 @@ def esx_assertion():
     }
 
     return data
+
+
+@pytest.fixture(scope='session')
+def rhevm_assertion():
+    """
+    Collect all the assertion info for rhevm to this fixture
+    :return:
+    """
+    login_error = 'Unable to connect to RHEV-M server: 401 Client Error'
+    data = {
+        'type': {
+            'invalid': {
+                'xxx': "Unsupported virtual type 'xxx' is set",
+                '红帽€467aa': "Unsupported virtual type '红帽€467aa' is set",
+                '': "Unsupported virtual type '' is set",
+            },
+            'non_rhel9': "virt-who can't be started",
+            'disable': "Cannot read CA certificate '/etc/pki/CA/cacert.pem'",
+            'disable_multi_configs': "Cannot read CA certificate '/etc/pki/CA/cacert.pem'"
+        },
+        'server': {
+            'invalid': {
+                'xxx': 'Name or service not known',
+                '红帽€467aa': 'Unable to connect to RHEV-M server',
+                '': 'Option server needs to be set in config',
+            },
+            'disable': "virt-who can't be started",
+            'disable_multi_configs': 'Required option: "server" not set',
+            'null_multi_configs': 'Option server needs to be set in config',
+        },
+        'username': {
+            'invalid': {
+                'xxx': login_error,
+                '红帽€467aa': login_error,
+                '': login_error,
+            },
+            'disable': 'Required option: "username" not set',
+            'disable_multi_configs': 'Required option: "username" not set',
+            'null_multi_configs': login_error,
+        },
+        'password': {
+            'invalid': {
+                'xxx': login_error,
+                '红帽€467aa': login_error,
+                '': login_error,
+            },
+            'disable': 'Required option: "password" not set',
+            'disable_multi_configs': 'Required option: "password" not set',
+            'null_multi_configs': login_error,
+        },
+        'encrypted_password': {
+            'invalid': {
+                'xxx': 'Option "encrypted_password" cannot be decrypted',
+                '': 'Option "encrypted_password" cannot be decrypted',
+            },
+            'valid_multi_configs': 'Option "encrypted_password" cannot be decrypted'
+        }
+
+    }
+
+    return data
