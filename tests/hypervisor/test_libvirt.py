c56c8c05ea9b43877c077318d3802af69aa5315c
hkx303 <kuhuang@redhat.com>
2023-03-23 00:12:21 +0800

    remove the test_guest_attr_by_curl from test_libvirt.py

--- tests/hypervisor/test_libvirt.py
+++ tests/hypervisor/test_libvirt.py
@@ -181,47 +181,6 @@ class TestLibvrtPositive:
         # Todo: Need to add the test cases for host-guest association in mapping, web and the test
         #  cases for the vdc pool's subscription.
 
-    @pytest.mark.tier1
-    def test_guest_attr_by_curl(self, virtwho, function_hypervisor, hypervisor_data,
-                                      register_data, rhsm, satellite, register_guest, ssh_host):
-        """
-        :title: virt-who: libvirt: check the guest attress by curl
-        :id: d9dd2559-4650-4ae0-8ebb-f8e296d3920a
-            1. Config the virt-who config file, run virt-who service
-            2. check guest attributes by curl
-
-        :expectedresults:
-            1. Succeed to run the virt-who service, no error messages in the rhsm.log file
-            2. Succeed to find the guest uuid in from the output of the curl command
-        """
-        host_name = hypervisor_data['hypervisor_hostname']
-        guest_uuid = hypervisor_data['guest_uuid']
-        guest_hostname = hypervisor_data['guest_hostname']
-        result = virtwho.run_service()
-        assert (result['error'] == 0
-                and result['send'] == 1
-                and result['thread'] == 1)
-
-        if REGISTER == 'rhsm':
-            registered_id = rhsm.uuid(host_name)
-            cmd = f"curl -s -k -u " \
-                f"{register_data['username']}:{register_data['password']} " \
-                f"https://{register_data['server']}/subscription/" \
-                f"consumers/{registered_id}/guestids/{guest_uuid}"
-            ret, output = ssh_host.runcmd(cmd)
-            assert (guest_uuid in output
-                    and "guestId" in output
-                    and "attributes" in output)
-        else:
-            guest_registered_id = satellite.host_id(guest_hostname)
-            cmd = f"curl -X GET -s -k -u " \
-                f"{register_data['username']}:{register_data['password']} " \
-                f"https://{register_data['server']}/api/v2/hosts/{guest_registered_id}"
-            ret, output = ssh_host.runcmd(cmd)
-            attr1 = f'"id":{guest_registered_id}'
-            assert (attr1 in output
-                    and guest_hostname in output)
-
 
 @pytest.mark.usefixtures('function_virtwho_d_conf_clean')
 @pytest.mark.usefixtures('class_debug_true')
