1053b103fdc3a5fe25879247122f444066d23fe6
hkx303 <kuhuang@redhat.com>
2023-03-23 21:52:54 +0800

    add test case test_associated_info_by_rhsmlog_and_webui

--- tests/hypervisor/test_default.py
+++ tests/hypervisor/test_default.py
@@ -53,3 +53,34 @@ class TestLibvrtPositive:
             attr1 = f'"id":{guest_registered_id}'
             assert (attr1 in output
                     and guest_hostname in output)
+
+    @pytest.mark.tier1
+    def test_associated_info_by_rhsmlog_and_webui(
+            self, virtwho, function_hypervisor, hypervisor_data, rhsm, satellite):
+        """
+        :title: virt-who: libvirt: check associated info by rhsm.log and webui
+        :id: cc0fd665-7154-4efa-ad71-b509b4224e22
+            1. Config the virt-who config file, run virt-who service
+            2. check host-to-guest association in rhsm.log/Web UI
+
+        :expectedresults:
+            1. Succeed to run the virt-who service, no error messages in the rhsm.log file
+            2. Succeed to find the host-to-guest association in rhsm.log/Web UI
+        """
+        host_name = hypervisor_data['hypervisor_hostname']
+        guest_uuid = hypervisor_data['guest_uuid']
+        guest_hostname = hypervisor_data['guest_hostname']
+
+        result = virtwho.run_service()
+        assert (result['error'] == 0
+                and result['send'] == 1
+                and result['thread'] == 1)
+
+        # check host-to-guest association in rhsm.log
+        assert guest_uuid in result['log']
+
+        # check host-to-guest association in webui
+        if REGISTER == 'rhsm':
+            assert rhsm.associate(host_name, guest_uuid)
+        else:
+            assert satellite.associate_on_webui(host_name, guest_hostname)
