3e75ad02875805c9afa9285a1d051d746d53dfb8
hkx303 <kuhuang@redhat.com>
2023-05-24 21:08:03 +0800

    add test case: test_threads_number_after_reconfig_libvirt

--- tests/hypervisor/test_local.py
+++ tests/hypervisor/test_local.py
@@ -7,40 +7,112 @@
 :caselevel: Component
 """
 import pytest
-from virtwho import logger
+import time
+
+from hypervisor import logger
 
 
-class TestLocal:
+@pytest.mark.usefixtures("function_virtwho_d_conf_clean")
+@pytest.mark.usefixtures("class_debug_true")
+@pytest.mark.usefixtures("class_globalconf_clean")
+class TestLocalPositive:
     @pytest.mark.tier1
-    def test_hostname_option(self):
-        """Just a demo
-
-        :title: virt-who: local: test hostname option
-        :id: 33cc5ba0-c529-481b-8cfa-8613adbe23ee
+    def test_threads_number_after_reconfig_libvirt(self, virtwho, ssh_host):
+        """
+        :title: virt-who: loacl: test threads number after reconfig libvirt config file
+        :id: 4d2552ce-af14-4e4f-84ab-64764db64fe2
         :caseimportance: High
         :tags: hypervisor,local,tier1
         :customerscenario: false
         :upstream: no
         :steps:
-            1.demo
-        :expectedresults:
-            1.demo
-        """
-        logger.info("Succeeded to run the 'test_hostname_option'")
+            1. Run virt-who service to check the thread number
+            2. Update libvirt config, and restart libvirtd service
+            3. Check virt-who thread_num is changed or not
+            4. Recovery libvirt config
 
-    @pytest.mark.tier2
-    def test_http_option(self):
-        """Just a demo
-
-        :title: virt-who: local: test http option
-        :id: a66787b3-1a5f-4b6f-9c09-0873d6490de3
-        :caseimportance: High
-        :tags: hypervisor,local,tier2
-        :customerscenario: false
-        :upstream: no
-        :steps:
-            1. demo
         :expectedresults:
-            1. demo
+            1. Succeeded to start virt-who service and the thread_num is 1
+            2. Succeed to start the libvirtd service
+            3. the thread_num is still 1
         """
-        logger.info("Succeeded to run the 'test_http_option'")
+
+        # run virt-who service to check the thread number
+        result = virtwho.run_service()
+        thread_berfore = result['thread']
+        logger.info(f"Succeeded to start virt-who service and the thread_num is {thread_berfore}")
+
+        # update libvirt config, and restart libvirtd service
+        libvirt_conf = "/etc/libvirt/libvirtd.conf"
+        option_enable("listen_tls", libvirt_conf, ssh_host)
+        option_enable("listen_tcp", libvirt_conf, ssh_host)
+        option_enable("auth_tcp", libvirt_conf, ssh_host)
+        option_enable("tcp_port", libvirt_conf, ssh_host)
+
+        run_service(ssh_host, "libvirtd", "restart")
+        _, output = run_service(ssh_host, "libvirtd", "status")
+        assert "is running" in output or "Active: active (running)" in output
+
+        # check virt-who thread_num is changed or not
+        thread_after = virtwho.thread_number()
+        assert thread_berfore == thread_after
+
+        # recovery libvirt config
+        option_disable("listen_tls", libvirt_conf, ssh_host)
+        option_disable("listen_tcp", libvirt_conf, ssh_host)
+        option_disable("auth_tcp", libvirt_conf, ssh_host)
+        option_disable("tcp_port", libvirt_conf, ssh_host)
+
+        run_service(ssh_host, "libvirtd", "restart")
+        _, output = run_service(ssh_host, "libvirtd", "status")
+        assert "is running" in output or "Active: active (running)" in output
+
+
+def option_enable(option, file, ssh_host):
+    """
+    Enable the option for the specfic file
+    :param option: the name of the option
+    :param file: the name of the file
+    :param ssh_host: the ssh host for running the command
+    :return:
+    """
+    cmd = f'sed -i "s|^#{option}|{option}|g" {file}'
+    ret, output = ssh_host.runcmd(cmd)
+    if ret == 0:
+        logger.info(f"Succeeded to enable option {option}")
+        return True
+    else:
+        logger.error(f"Failed to enable option {option}")
+        return False
+
+
+def option_disable(option, file, ssh_host):
+    """
+    Disable the option for the specfic file
+    :param option: the name of the option
+    :param file: the name of the file
+    :param ssh_host: the ssh host for running the command
+    :return: Succeed to run the command, return True; Else, return False
+    """
+    cmd = f'sed -i "s|^{option}|#{option}|g" {file}'
+    ret, output = ssh_host.runcmd(cmd)
+    if ret == 0:
+        logger.info(f"Succeeded to disable option {option}")
+        return True
+    else:
+        logger.error(f"Failed to disable option {option}")
+        return False
+
+
+def run_service(ssh_host, name, action):
+    """
+    Run the service command, such as service virt-who start
+    :param ssh_host: the ssh host for running the command
+    :param name: the name of the service
+    :param action: Start, Stop, Status
+    :return: Succeed to run the command, return True; Else, return False
+    """
+    cmd = f"service {name} {action}"
+    ret, output = ssh_host.runcmd(cmd)
+    time.sleep(10)
+    return ret, output
