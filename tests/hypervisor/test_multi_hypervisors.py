feb969384f70e7ce796336dafb69d9a92ae6cb7c
hkx303 <kuhuang@redhat.com>
2023-06-21 18:29:05 +0800

    add test case to test_multi_hypervisors.py

--- tests/hypervisor/test_multi_hypervisors.py
+++ tests/hypervisor/test_multi_hypervisors.py
@@ -6,11 +6,17 @@
 :subsystemteam: sst_subscription_virtwho
 :caselevel: Component
 """
-from virtwho import logger
+import pytest
+
+from virtwho.settings import config
+from virtwho.configure import hypervisor_create
 
 
+@pytest.mark.usefixtures("function_virtwho_d_conf_clean")
+@pytest.mark.usefixtures("class_debug_true")
+@pytest.mark.usefixtures("class_globalconf_clean")
 class TestMultiHypervisors:
-    def test_multi_hypervisors_report_together(self):
+    def test_multi_hypervisors_report_together(self, virtwho):
         """Test virt-who can report multi hypervisors together
 
         :title: virt-who: multiHypervisors: test multi hypervisors report together
@@ -20,9 +26,13 @@ class TestMultiHypervisors:
         :customerscenario: false
         :upstream: no
         :steps:
-            1. demo
+            1. Create the virt-who config files for the multi hypervisors list
+            2. Run the virt-who service
+
         :expectedresults:
-            1. demo
+            1. Succeed to run the virt-who, no error messages in the rhsm.log
         """
-        logger.info("Succeeded to run the 'test_multi_hypervisors_report'")
-        assert True
+        for mode in config.job.multi_hypervisors.strip('[').strip(']').split(','):
+            hypervisor_create(mode)
+        result = virtwho.run_service()
+        assert result["error"] == 0 and result["send"] == 1 and result["thread"] == 1
