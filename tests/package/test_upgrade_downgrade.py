d409948639ed2104221be7c8da44ad1e1581beb5
hkx303 <kuhuang@redhat.com>
2023-06-27 13:58:42 +0800

    skip the test case for local mode

--- tests/package/test_upgrade_downgrade.py
+++ tests/package/test_upgrade_downgrade.py
@@ -29,7 +29,8 @@ if "RHEL-8" in RHEL_COMPOSE:
 @pytest.mark.usefixtures("function_host_register_for_local_mode")
 class TestUpgradeDowngrade:
     @pytest.mark.tier1
-    def test_upgrade_downgrade_by_yum(self, ssh_host, virtwho, globalconf, rhsm):
+    @pytest.mark.notLocal
+    def test_upgrade_downgrade_by_yum(self, ssh_host, virtwho, globalconf):
         """Test virt-who upgrade/downgrade by yum
 
         :title: virt-who: package: upgrade/downgrade by yum
@@ -63,7 +64,6 @@ class TestUpgradeDowngrade:
                 compose_path=old_compose_path,
             )
             # downgrade virt-who to check the configurations not change.
-            rhsm.sca(sca="disable")
             package_downgrade(ssh_host, "virt-who")
             result = virtwho.run_service()
             assert (
