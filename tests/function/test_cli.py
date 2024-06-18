90ce65a438434f3c60edc4bba456778e52eed793
YuefenLiu <yuefliu@redhat.com>
2024-06-18 14:26:57 +0800

    an update

--- tests/function/test_cli.py
+++ tests/function/test_cli.py
@@ -134,7 +134,7 @@ class TestCli:
             rhsm_log = virtwho.rhsm_log_get()
             assert "No data to send, waiting for next interval" in rhsm_log
         else:
-            assert result["loop"] in [60, 61, 62, 63]
+            assert 60 <= result["loop"] <= 63
 
     @pytest.mark.tier1
     def test_print(self, virtwho, hypervisor_data):
--- tests/function/test_config.py
+++ tests/function/test_config.py
@@ -86,7 +86,7 @@ class TestConfigurationPositive:
             rhsm_log = virtwho.rhsm_log_get()
             assert "No data to send, waiting for next interval" in rhsm_log
         else:
-            assert result["loop"] in [60, 61, 62, 63]
+            assert 60 <= result["loop"] <= 63
 
     @pytest.mark.tier1
     @pytest.mark.fedoraSmoke
