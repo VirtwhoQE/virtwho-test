743b0bf1b30b77620c923c960d69c91ebef54ad7
YuefenLiu <yuefliu@redhat.com>
2024-06-21 09:53:49 +0800

    fix interval case loop issue

--- tests/function/test_config.py
+++ tests/function/test_config.py
@@ -86,7 +86,7 @@ class TestConfigurationPositive:
             rhsm_log = virtwho.rhsm_log_get()
             assert "No data to send, waiting for next interval" in rhsm_log
         else:
-            assert 60 <= result["loop"] <= 63
+            assert 59 <= result["loop"] <= 63
 
     @pytest.mark.tier1
     @pytest.mark.fedoraSmoke
