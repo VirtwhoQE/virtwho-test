cb744e19e5fd961a116f12629cf288df9a5dc0d0
Jan Stavěl @ RedHat <jstavel@redhat.com>
2025-04-08 07:14:10 +0200

    CCT-1273: syntax error

--- virtwho/ssh.py
+++ virtwho/ssh.py
@@ -68,7 +68,7 @@ class SSHConnect:
         # transport.connect(username=self.user, password=self.pwd)
         # sftp = paramiko.SFTPClient.from_transport(transport)
         # return sftp, transport
-       try:
+        try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(self.host, self.port, self.user, self.pwd, timeout=self.timeout)
